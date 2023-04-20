8f2a70d03932593e1b98aca9b5c04de4815fe3ab
Carlos Arjonilla <arj20001@byui.edu>
2023-04-20 17:14:08 -0600

    finished prep1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // asks user to enter thier first name
+        Console.WriteLine("What is your first name?");
+        string first_name = Console.ReadLine();
+
+        // asks user to enter thier last name
+        Console.WriteLine("What is your last name?");
+        string last_name = Console.ReadLine();
+
+        // spacer
+        Console.WriteLine("");
+
+        // prints the users first and last name in a james bond format
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
