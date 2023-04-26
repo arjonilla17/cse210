ef2cd16137961d88a411e5761b9089cabe5ac16f
Carlos Arjonilla <arj20001@byui.edu>
2023-04-26 12:28:59 -0600

    abstract-example

---
+++ abstract-example/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Book mistborn = new Book("Mistborn","Brandon Sanderson");
+
+mistborn.Display();
+
+Book Mazerunner = new Book("MazeRunner", "James Dashner");
+
+Mazerunner.Display();
+
+Console.WriteLine(mistborn.IsAvailable());
+Console.WriteLine(Mazerunner.IsAvailable());
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // asks the user for a number
+        Console.Write("What is the magic number?");
+        string magic_number = Console.ReadLine();
+        int magic_number_int  = int.Parse(magic_number);
+        
+        Console.WriteLine("What is your guess?");
+        string guess = Console.ReadLine();
+        int user_guess = int.Parse(guess);
+
+        while (user_guess != magic_number_int)
+        {
+            if (magic_number_int > user_guess)
+            {
+                Console.WriteLine("Higher");
+                
+            }
+
+            else if (magic_number_int < user_guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed the magic number!");
+            }
+            
+        }
+
+       
+
     }
 }
