830f78875b4d1517b1deddbadcf5e224a7a32418
Carlos Arjonilla <arj20001@byui.edu>
2023-05-02 15:19:16 -0600

    Classes with Cars

---
+++ prepare/Learning02/Car.cs
@@ -0,0 +1,35 @@
+public class Car {
+
+// instances
+public string brand;
+public int miles;
+public string color;
+// constructor
+public Car(){
+}
+
+
+// methods (functions)
+public void move_forward(){
+
+    Console.Write("Is running");
+}
+
+public void displayInfo(){
+    Console.WriteLine($"brand: {brand} Miles: {miles} Color: {color}");
+}
+
+}
+
+//  Console.Write("Hello World");
+
+        // Car bmw_m3 = new Car();
+
+        // bmw_m3.brand = "BMW";
+        // bmw_m3.miles = 1000;
+        // bmw_m3.color = "Black";
+
+        // bmw_m3.displayInfo();
+        // bmw_m3.move_forward();
+
+        // Job 
