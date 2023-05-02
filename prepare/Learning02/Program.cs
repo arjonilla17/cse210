bcead22f5a24c89aebc62e65bbb62d80cb787f27
Carlos Arjonilla <arj20001@byui.edu>
2023-05-02 15:20:18 -0600

    displays classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+      Job job1 = new Job();
+      job1._company = "Jani-King";
+      job1._jobTitle = "Janitor";
+      job1._startYear = 2006;
+      job1._endYear = 2020;
+
+      Job job2 = new Job();
+      job2._company = "USL";
+      job2._jobTitle = "Data Analyst";
+      job2._startYear = 2020;
+      job2._endYear = 2022;
+
+      Resume ResumeList = new Resume();
+      ResumeList._name = "Carlos Arjonilla";
+
+      ResumeList._jobs.Add(job1);
+      ResumeList._jobs.Add(job2);
+
+      ResumeList.DisplayResume();
+
     }
 }
